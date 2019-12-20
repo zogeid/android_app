a8d66b59211798478c9674b78cfd6967b84c490d
dalares <zogeid@gmail.com>
2019-12-20 15:43:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android_app/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.android_app;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); //invoca constructor de la clase padre
+        Log.i("TAG cicloVida1", "ciclo vida on create");
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.e("TAG cicloVida2", "ciclo vida on start");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.w("TAG cicloVida3", "ciclo vida on resume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.w("TAG cicloVida4", "ciclo vida on pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("TAG cicloVida5", "ciclo vida on stop");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will automatically handle clicks on
+        // the Home/Up button, so long as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
